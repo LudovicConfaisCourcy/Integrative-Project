f32366e5c3cf8572fb87cb2c204566f56574d936
LudovicConfaisCourcy <123512499+LudovicConfaisCourcy@users.noreply.github.com>
2024-03-08 08:38:15 -0500

    Open window when button pressed

--- src/main/java/edu/vanier/template/controllers/FXMLMainAppController.java
+++ src/main/java/edu/vanier/template/controllers/FXMLMainAppController.java
@@ -3,6 +3,9 @@ package edu.vanier.template.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,19 +25,51 @@ public class FXMLMainAppController {
     Button btnCredits;
     @FXML
     Button btnQuit;
-    
     @FXML
     Label welcomeLabel;
+    @FXML
+    Label welcomeLabel1;
  
     @FXML
     public void initialize() {
         logger.info("Initializing MainAppController...");
         btnPlay.setOnAction((event) -> {
-            handleClickMe();
+            handleClickMePlay();      
         });
+        btnSettings.setOnAction((event) -> {
+            handleClickMeSettings();      
+        });
+        btnCredits.setOnAction((event) -> {
+            handleClickMeCredits();      
+        });
+        
     }
     @FXML
-    private void handleClickMe() {
-        logger.info("Clicked Play Button");        
+    private void handleClickMePlay() {
+        logger.info("Clicked Play Button");    
+         VBox mainVbox = new VBox();
+        Scene secondScene = new Scene(mainVbox);
+        Stage secondStage = new Stage();
+        secondStage.setScene(secondScene);
+        secondStage.setTitle("Play");
+        secondStage.show();
+    }
+    private void handleClickMeSettings() {
+        logger.info("Clicked Play Button");    
+         VBox mainVbox = new VBox();
+        Scene secondScene = new Scene(mainVbox);
+        Stage secondStage = new Stage();
+        secondStage.setScene(secondScene);
+        secondStage.setTitle("Settings");
+        secondStage.show();
+    }
+    private void handleClickMeCredits() {
+        logger.info("Clicked Play Button");    
+         VBox mainVbox = new VBox();
+        Scene secondScene = new Scene(mainVbox);
+        Stage secondStage = new Stage();
+        secondStage.setScene(secondScene);
+        secondStage.setTitle("Credits");
+        secondStage.show();
     }
 }
