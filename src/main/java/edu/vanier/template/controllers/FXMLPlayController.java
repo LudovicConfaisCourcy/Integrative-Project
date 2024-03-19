cd0d2a7731b65cd31f357501ebb0b8c4ef5927e7
AntoxaLis <123769950+AntoxaLis@users.noreply.github.com>
2024-03-19 13:47:49 -0400

    Main App Optimased in one method

--- src/main/java/edu/vanier/template/controllers/FXMLPlayController.java
+++ src/main/java/edu/vanier/template/controllers/FXMLPlayController.java
@@ -6,13 +6,32 @@ import java.util.logging.Level;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * @authors
- *  Ludovic Confais Courcy
+ * @authors 
  *  Anton Lisunov
- *  Shyam Patel
  */
 public class FXMLPlayController {
 
+    private final static Logger logger = LoggerFactory.getLogger(FXMLMainAppController.class);
+    @FXML
+    Button btnPlay;
+    @FXML
+    Button btnStop;
+    @FXML
+    Button btnRestarts;
+    @FXML
+    Button btnMenu;
+    @FXML
+    Button btnHelp;
+    @FXML
+    Label lbScore;
+    @FXML
+    Pane pnNext;
+    
+    
 }
