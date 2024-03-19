db1652e0e353dde982c024c74417d32a966dd376
AntoxaLis <123769950+AntoxaLis@users.noreply.github.com>
2024-03-19 14:21:24 -0400

    Play menu Mod.

--- src/main/java/edu/vanier/template/controllers/FXMLPlayController.java
+++ src/main/java/edu/vanier/template/controllers/FXMLPlayController.java
@@ -1,7 +1,7 @@
-
 package edu.vanier.template.controllers;
 
 import edu.vanier.template.controllers.FXMLMainAppController;
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -11,61 +11,57 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * @authors 
- *  Anton Lisunov
+ * @authors Anton Lisunov
  */
 public class FXMLPlayController {
 
-    private final static Logger logger = LoggerFactory.getLogger(FXMLMainAppController.class);
-    @FXML
-    Button btnPlay;
-    @FXML
-    Button btnStop;
-    @FXML
-    Button btnRestarts;
-    @FXML
-    Button btnMenu;
-    @FXML
-    Button btnHelp;
-    @FXML
-    Label lbScore;
-    @FXML
-    Pane pnNext;
-    
-    @FXML
-    public void initialize() {
+  private final static Logger logger = LoggerFactory.getLogger(FXMLMainAppController.class);
+@FXML
+Button btnPlay;
+@FXML
+Button btnStop;
+@FXML
+Button btnRestart;
+@FXML
+Button btnMenu;
+@FXML
+Button btnHelp;
+@FXML
+Label lbScore;
+@FXML
+Pane pnNext;
 
-    }
-    
-    @FXML
-    private void handleBtnPlay(ActionEvent event) {
-        logger.info("Play button clicked");
-        // Implement the functionality to stop the game or any other action
-    }
-    
-    @FXML
-    private void handleBtnStop(ActionEvent event) {
-        logger.info("Stop button clicked");
-        // Implement the functionality to stop the game or any other action
-    }
-    
-    @FXML
-    private void handleBtnRestart(ActionEvent event) {
-        logger.info("Restart button clicked");
-        // Implement the functionality to restart the game or any other action
-    }
-    
-    @FXML
-    private void handleBtnMenu(ActionEvent event) {
-        logger.info("Menu button clicked");
-        // Implement the functionality to go to the menu or any other action
-    }
-    
-    @FXML
-    private void handleBtnHelp(ActionEvent event) {
-        logger.info("Help button clicked");
-        // Implement the functionality to show help or any other action
-    }
-    
-    
+@FXML
+public void initialize() {
+}
+
+@FXML
+private void handleBtnPlay() {
+    logger.info("Play button clicked");
+    // Implement the functionality to start or resume the game
+}
+
+@FXML
+private void handleBtnStop() {
+    logger.info("Stop button clicked");
+    // Implement the functionality to stop the game
+}
+
+@FXML
+private void handleBtnRestart() {
+    logger.info("Restart button clicked");
+    // Implement the functionality to restart the game
+}
+
+@FXML
+private void handleBtnMenu() {
+    logger.info("Menu button clicked");
+    // Implement the functionality to go to the menu
+}
+
+@FXML
+private void handleBtnHelp() {
+    logger.info("Help button clicked");
+    // Implement the functionality to show help
+}
 }
