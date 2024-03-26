2418c46f532a2f6992ef7398637d27402e3b5e73
LudovicConfaisCourcy <2163918@edu.vaniercollege.qc.ca>
2024-03-26 13:57:53 -0400

    d

--- src/main/java/edu/vanier/template/controllers/FXMLCreditsController.java
+++ src/main/java/edu/vanier/template/controllers/FXMLCreditsController.java
@@ -2,11 +2,18 @@
 package edu.vanier.template.controllers;
 
 
+import edu.vanier.template.MainApp;
 import java.io.IOException;
 import java.util.logging.Level;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
  /* @authors
  *  Ludovic Confais Courcy
@@ -16,6 +23,7 @@ import javafx.scene.control.Button;
 
    
 public class FXMLCreditsController {
+    private final static Logger logger = LoggerFactory.getLogger(FXMLMainAppController.class);
     @FXML
     Button btnCreators;
     @FXML
@@ -25,4 +33,29 @@ public class FXMLCreditsController {
     @FXML
     Button btnDescription;   
 
-}
+    @FXML
+    public void switchScenes(ActionEvent event) throws IOException {
+        Object clickButton = event.getSource();
+        if (clickButton == btnCreators) {
+            logger.info("Clicked Play Button");
+            switchScene("/fxml/TetrisScene.fxml", new FXMLPlayController());
+        } else if (clickButton == btnInspirations) {
+            logger.info("Clicked Settng Button");
+            switchScene("/fxml/SettingsScene.fxml", new FXMLSettingsController());
+        } else if (clickButton == btnScience) {
+            logger.info("Clicked Credit Button");
+            switchScene("/fxml/CreditScene.fxml", new FXMLCreditsController());
+        } else if (clickButton == btnDescription) {
+            logger.info("Clicked Quit Button");
+            Stage currentStage = (Stage) btnCreators.getScene().getWindow();
+        }
+    }
+
+    public void switchScene(String fxml, Object controller) throws IOException {
+        logger.info("Stage is changed");
+        Stage currentStage = (Stage) btnCreators.getScene().getWindow();
+        MainApp mainApp = new MainApp();
+        FXMLLoader loader = mainApp.loadFXML(fxml, controller);
+        currentStage.setScene(new Scene(loader.load()));
+    
+}}
