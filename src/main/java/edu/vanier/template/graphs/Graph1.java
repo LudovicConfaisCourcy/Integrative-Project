260e71386f92be871ac7667b4ddf85bf0d1df841
LudovicConfaisCourcy <123512499+LudovicConfaisCourcy@users.noreply.github.com>
2024-04-19 08:34:19 -0400

    Update Graph1.java

--- src/main/java/edu/vanier/template/graphs/Graph1.java
+++ src/main/java/edu/vanier/template/graphs/Graph1.java
@@ -4,6 +4,8 @@
  */
 package edu.vanier.template.graphs;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
@@ -15,12 +17,12 @@ import javafx.stage.Stage;
  * @author 2246745
  */
 public class Graph1 {
-    
+    private AtomicInteger time = new AtomicInteger(0);
     public Graph1 (){
      Stage graphStage = new Stage();
         graphStage.setTitle("Line Chart Example");
 
-        final NumberAxis xAxis = new NumberAxis();
+        /*final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
         xAxis.setLabel("X Axis");
         yAxis.setLabel("Y Axis");
@@ -48,6 +50,47 @@ public class Graph1 {
 
         Scene scene = new Scene(lineChart, 800, 600);
         graphStage.setScene(scene);
-        graphStage.show();}
+        graphStage.show();}*/
+    final NumberAxis xAxis = new NumberAxis();
+    final NumberAxis yAxis = new NumberAxis();
+
+    xAxis.setAnimated(false);
+    xAxis.setLabel("Time (s)");
+
+    yAxis.setAnimated(false);
+    yAxis.setLabel("Position");
+
+    XYChart.Series<Number, Number> series = new XYChart.Series<>();
+    series.setName("Values");
+
+    LineChart<Number, Number> chart = new LineChart<>(xAxis, yAxis);
+    chart.setAnimated(false);
+    chart.getData().add(series);
+
+    Scene scene = new Scene(chart, 620, 350);
+    graphStage.setScene(scene);
+    graphStage.show();
+    UpdatewithTime(series);
     
+    }
+    
+    
+    public void UpdatewithTime(XYChart.Series<Number, Number> series){
+        Thread updateThreadwithTime = new Thread(() -> {
+      
+    while (true) {
+        
+        try {
+          Thread.sleep(1000);
+          Platform.runLater(() -> series.getData().add(new XYChart.Data<>(time.incrementAndGet()- 1, (int) (Math.random() * 100 ))));
+        }
+        
+        catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
+      }
+    });   
+    updateThreadwithTime.start();
+  } 
+
 }
