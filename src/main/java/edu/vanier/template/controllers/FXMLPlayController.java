582b7628de21d003b096c33271e1a3c7e8774490
Ludocfs <122496032+Ludocfs@users.noreply.github.com>
2024-05-09 22:56:02 -0400

    fix issue + bugs

--- src/main/java/edu/vanier/template/controllers/FXMLPlayController.java
+++ src/main/java/edu/vanier/template/controllers/FXMLPlayController.java
@@ -1,26 +1,34 @@
 package edu.vanier.template.controllers;
-
+import javafx.scene.paint.Color;
+import library.dynamics.Body;
+import library.geometry.Polygon;
+import library.joints.Joint;
+import library.joints.JointToBody;
+import library.math.Vectors2D;
 import edu.vanier.template.MainApp;
 import edu.vanier.template.graphs.Graph1;
+import edu.vanier.template.tetrisPieces.TetrisShapes;
 import edu.vanier.template.physicalLaws.Physics;
 import edu.vanier.template.tetrisPieces.BlockState;
 import edu.vanier.template.tetrisPieces.TetrisBlock;
 import edu.vanier.template.tetrisPieces.TetrisGround;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Random;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import library.dynamics.Body;
-import library.math.Vectors2D;
+import library.geometry.Polygon;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,7 +39,7 @@ public class FXMLPlayController {
 
     private final static Logger logger = LoggerFactory.getLogger(FXMLMainAppController.class);
     Physics physics;
-
+    Body body;
     FXMLSettingsController music = new FXMLSettingsController();
     @FXML
     Button btnPlay;
@@ -51,16 +59,38 @@ public class FXMLPlayController {
     Pane pnBoard;
     @FXML
     BorderPane BorderPane;
-
+    Random random = new Random();
+    int rand = random.nextInt(6);
+    @FXML
+    Group GroupLBlock1;
+    @FXML
+    Group GroupSquareBlock;
+    @FXML
+    Group GroupSBlock1;
+    @FXML
+    Group GroupLineBlock1;
+    @FXML
+    Group GroupTBlock;
+    @FXML
+    Group GroupZBlock;
+    @FXML
+    Group GroupJBlock;
+    @FXML
+    Label PlayScreenLabel;
+    double width;
+    double height;
     @FXML
     public void initialize() {
-        physics = new Physics(pnBoard);
-        for (int i = 0; i < 5; i++) {
-            physics.addGround(new TetrisGround(60, 40, Color.GREEN), 0, -150);
 
+        physics = new Physics(pnBoard);
+        for (int i = 0; i < 1; i++) {
+            physics.addGround(new TetrisGround(60, 40, Color.GREEN), 0, -150);
         }
 
 
+        /*if(  this.position.x >= pnBoard.getWidth()  || pnBoard.getHeight() >= 100){
+            System.out.println("yes");
+        }*/
         /*pnBoard.widthProperty().addListener((obs, oldWidth, newWidth) -> {
             ground.setWidth(newWidth.doubleValue() * 0.5);
             ground.setTranslateX(newWidth.doubleValue() * 0.25);
@@ -70,46 +100,89 @@ public class FXMLPlayController {
             ground.setHeight(newHeight.doubleValue() * 0.3);
             ground.setTranslateY(newHeight.doubleValue() * 0.9);
         });*/
+        setInvisible();
         music.musicPlay();
 
     }
 
+    public void setInvisible(){
+        GroupSquareBlock.setVisible(false);
+        GroupLBlock1.setVisible(false);
+        GroupSBlock1.setVisible(false);
+        GroupTBlock.setVisible(false);
+        GroupLineBlock1.setVisible(false);
+        GroupZBlock.setVisible(false);
+        GroupJBlock.setVisible(false);
+    }
+
     @FXML
     private void handleBtnPlay() {
-
         logger.info("Start button clicked");
-        music.soundPlay();
+        PlayScreenLabel.setText("Click Stop to Pause the Game");
         physics.startPhysics();
-        physics.addTetrisShape('I', new TetrisBlock(10, 10, Color.RED), 0, 150);
-        /* physics.addTetrisShape('L', new TetrisBlock(10, 10, Color.RED), 0, 250);
-        physics.addTetrisShape('J', new TetrisBlock(10, 10, Color.RED), 0, 350);
-        physics.addTetrisShape('T', new TetrisBlock(10, 10, Color.RED), 0, 450);
-        physics.addTetrisShape('O', new TetrisBlock(10, 10, Color.RED), 0, 550);
-        physics.addTetrisShape('S', new TetrisBlock(10, 10, Color.RED), 0, 650);
-        physics.addTetrisShape('Z', new TetrisBlock(10, 10, Color.RED), 0, 750);*/
-
-        System.out.println(physics.getBlocks().size());
-
-        for (int i = 5; i < physics.getBodies().size(); i += 4) {
-            ArrayList<Rectangle> blocks = new ArrayList<>(physics.getBlocks().subList(i, i + 4));
-            ArrayList<Body> bodies = new ArrayList<>(physics.getBodies().subList(i, i + 4));
-
-            moveBlock(blocks, bodies);
+        if(physics.GameLostVerifier() == true){
+            PlayScreenLabel.setText("!!You Lost!! Click Play to ReStart a Game");
         }
+        music.soundPlay();
+        rand += 1;
+        rand %= 7;
+        System.out.println(rand);
+
+        switch(rand){
+            case 0 : physics.addTetrisShape('I', new TetrisBlock(10, 10, Color.RED), 0, 150, lbScore);
+                setInvisible();
+                GroupLBlock1.setVisible(true);
+                break;
+            case 1 : physics.addTetrisShape('L', new TetrisBlock(10, 10, Color.RED), 0, 150, lbScore);
+                setInvisible();
+                GroupJBlock.setVisible(true);
+                break;
+            case 2 : physics.addTetrisShape('J', new TetrisBlock(10, 10, Color.RED), 0, 150,lbScore);
+                setInvisible();
+                GroupTBlock.setVisible(true);
+                break;
+            case 3 : physics.addTetrisShape('T', new TetrisBlock(10, 10, Color.RED), 0, 150,lbScore);
+                setInvisible();
+                GroupSquareBlock.setVisible(true);
+                break;
+            case 4 : physics.addTetrisShape('O', new TetrisBlock(10, 10, Color.RED), 0, 150,lbScore);
+                setInvisible();
+                GroupSBlock1.setVisible(true);
+                break;
+            case 5 : physics.addTetrisShape('S', new TetrisBlock(10, 10, Color.RED), 0, 150,lbScore);
+                setInvisible();
+                GroupZBlock.setVisible(true);
+                break;
+            case 6 : physics.addTetrisShape('Z', new TetrisBlock(10, 10, Color.RED), 0, 150,lbScore);
+                setInvisible();
+                GroupLineBlock1.setVisible(true);
+                break;
+            default : System.out.println("error");
+        }
+
     }
 
     @FXML
     private void handleBtnStop() {
         logger.info("Stop button clicked");
+        PlayScreenLabel.setText("Click Play to UnPause the Game");
         music.soundPlay();
         physics.stopPhysics();
+
     }
 
     @FXML
     private void handleBtnRestart() {
         logger.info("Restart button clicked");
+        PlayScreenLabel.setText("Click Play to ReStart the Game");
+        physics.startPhysics();
+        lbScore.setText("0000");
         music.soundPlay();
-        pnBoard.getChildren().clear();
+        physics.removeAll();
+        for (int i = 0; i < 1; i++) {
+            physics.addGround(new TetrisGround(60, 40, Color.GREEN), 0, -150);
+
+        }
 
     }
 
@@ -168,44 +241,36 @@ public class FXMLPlayController {
         }
     }
 
-    public void moveBlock(ArrayList<Rectangle> blocks, ArrayList<Body> bodies) {
-        double paneWidth = 400;
-        double paneHeight = 380;
+    public void MoveBlock(TetrisBlock block) {
 
-        for (int i = 0; i < blocks.size(); i++) {
-            Rectangle block = blocks.get(i);
-            Body body1 = bodies.get(i);
-            Body body2 = bodies.get(Math.abs(i + 1 - 4));
-            Body body3 = bodies.get(Math.abs(i + 2 - 4));
-            Body body4 = bodies.get(Math.abs(i + 3 - 4));
+        /* block.setOnMousePressed(event -> {
 
-            Vectors2D difference2 = body2.position.subtract(body1.position);
-            Vectors2D difference3 = body3.position.subtract(body1.position);
-            Vectors2D difference4 = body4.position.subtract(body1.position);
+            BlockState state = new BlockState(block.getCurrentState().getPosX(), block.getCurrentState().getPosY(), block.getCurrentState().getSpeedX(), block.getCurrentState().getSpeedY(), 0, 0);
+            block.addBlockState(state);
+            BorderPane.setCursor(Cursor.CLOSED_HAND);
+        });
 
-            if (!body1.isStatic()) {
-                block.setOnMousePressed(event -> {
-                    BorderPane.setCursor(Cursor.CLOSED_HAND);
-                });
-            }
+        block.setOnMouseDragged(event -> {
 
-            block.setOnMouseDragged(event -> {
-                body1.position = new Vectors2D(event.getSceneX() - paneWidth / 2 - block.getWidth() * 4,
-                        -event.getSceneY() + paneHeight / 2 + block.getHeight() * 2);
+            block.setManaged(false);
+            BlockState state = new BlockState(event.getX() + block.getCurrentState().getPosX() - block.getWidth() / 2, event.getY() + block.getCurrentState().getPosY() - block.getHeight() / 2, block.getCurrentState().getSpeedX(), block.getCurrentState().getSpeedY(), 0, 0);
+            block.addBlockState(state);
+            event.consume();
+            physics.stopPhysics();
+        });
 
-                body2.position = new Vectors2D(body1.position.x+difference2.x, body1.position.y+difference2.y);
-                body3.position = new Vectors2D(body1.position.x+difference3.x, body1.position.y+difference3.y);
-                body4.position = new Vectors2D(body1.position.x+difference4.x, body1.position.y+difference4.y);
+        block.setOnMouseReleased(event -> {
 
-                event.consume();
-
-            });
-
-            block.setOnMouseReleased(event -> {
-                BorderPane.setCursor(Cursor.OPEN_HAND);
-            });
-        }
-
+            // Calculate velocity based on the change in position
+            double speedX = (block.getCurrentState().getPosX() - block.getPreviousState().getPosX()) / 0.16;
+            double speedY = (block.getCurrentState().getPosY() - block.getPreviousState().getPosY()) / 0.16;
+            BlockState state = new BlockState(block.getCurrentState().getPosX(), block.getCurrentState().getPosY(), speedX, speedY, 0, 0);
+            block.addBlockState(state);
+            physics.startPhysics();
+            BorderPane.setCursor(Cursor.OPEN_HAND);
+            // Start physics simulation
+        });
+         */
     }
 
     @FXML
